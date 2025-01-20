9be2fef02d1f25c9ff50fb5cecf53ef60f4c7492
pernillasterner <psterner@hotmail.com>
2025-01-20 21:43:20 +0100

    Add footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         brightRedSupLight: 'hsl(12, 88%, 95%)',
         darkBlue: 'hsl(227, 12%, 61%)',
         darkGrayishBlue: 'hsl(227, 12%, 61%)',
-        veryDarkBlue: 'hsl(223; 12%, 13%)',
+        veryDarkBlue: 'hsl(223, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
         veryLightGray: 'hsl(0, 0%, 98%)',
       }
